970f37d6a50e2a6aebfe7241b0a395704fa01f61
vishwakarma <vishwakarma2127@gmail.com>
2022-05-15 17:42:28 +0530

    added square.cpp

---
+++ Assigment2/square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{   
+    int l,b;
+    cout<<"Enter the length :";
+    cin>>l;
+    cout<<"Enter the breath :";
+    cin>>b;
+    if(l==b)
+    cout<<"It is square";
+    else
+    cout<<"It is not a square";
+    return 0;
+}
